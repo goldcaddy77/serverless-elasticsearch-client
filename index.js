31b435c880c4f8fbc50ce7899bf2bcd64b1b7ae3
Dan Caddigan <goldcaddy77@gmail.com>
2017-09-05 15:16:27 -0400

    feat(prettier): switch to prettier (#2)

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
-'use strict'
+'use strict';
 
-const AWS = require('aws-sdk')
-const elasticsearch = require('elasticsearch')
-const httpAWSESClass = require('http-aws-es')
+const AWS = require('aws-sdk');
+const elasticsearch = require('elasticsearch');
+const httpAWSESClass = require('http-aws-es');
 
 module.exports = {
   createClient: createClient,
   getOptions: getOptions
-}
+};
 
-function getOptions (options) {
-  options = options || {}
+function getOptions(options) {
+  options = options || {};
 
   // serverless-offline will set IS_OFFLINE based on whether we're offline
-  const devMode = Boolean(process.env.IS_OFFLINE)
+  const devMode = Boolean(process.env.IS_OFFLINE);
 
-  const prefix = options.envPrefix || 'AWS'
-  const region = options.region || process.env[`${prefix}_REGION`]
-  const host = options.host || process.env[`${prefix}_HOST`]
+  const prefix = options.envPrefix || 'AWS';
+  const region = options.region || process.env[`${prefix}_REGION`];
+  const host = options.host || process.env[`${prefix}_HOST`];
 
-  delete (options.region) // this doesn't belong in ES options
+  delete options.region; // this doesn't belong in ES options
 
-  if (!region) { throw new TypeError('region is required') }
-  if (!host) { throw new TypeError('host is required') }
+  if (!region) {
+    throw new TypeError('region is required');
+  }
+  if (!host) {
+    throw new TypeError('host is required');
+  }
 
-  const credentials = options.credentials || new AWS.EnvironmentCredentials(prefix)
+  const credentials = options.credentials || new AWS.EnvironmentCredentials(prefix);
 
   const config = Object.assign({}, options, {
     host: host,
@@ -32,16 +36,16 @@ function getOptions (options) {
       region,
       credentials
     }
-  })
+  });
 
   // don't sign the request in offline mode
   if (!devMode) {
-    config.connectionClass = httpAWSESClass
+    config.connectionClass = httpAWSESClass;
   }
 
-  return config
+  return config;
 }
 
-function createClient (options) {
-  return new elasticsearch.Client(getOptions(options))
+function createClient(options) {
+  return new elasticsearch.Client(getOptions(options));
 }
